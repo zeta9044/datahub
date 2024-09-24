e8083f1d3ee92e7846f71beb4bd001dc562bedae
kangyou.choi <kangyou.choi@datastreams.co.kr>
2024-09-24 04:58:29 +0000

    Add convert_sqlsrc.py for SQL source to JSON conversion.
    This script introduces a function to convert SQL source files to JSON format within a specific project. It handles environment variable validation, file path checks, and sets up and runs a conversion pipeline using DataHub.

---
+++ metadata-ingestion/src/zeta_lab/pipeline/convert_sqlsrc.py
@@ -0,0 +1,54 @@
+import os
+from zeta_lab.source.sqlsrc_to_json_converter import SqlsrcToJSONConverter
+from datahub.ingestion.run.pipeline import Pipeline
+
+def convert_sqlsrc(prj_id):
+    """
+    :param prj_id: Project identifier used to locate specific project files in the repository.
+    :return: None
+    """
+    try:
+        # repository path
+        engine_home = os.getenv("LIAENG_HOME")
+        if not engine_home:
+            raise ValueError("Please define environment variable 'LIAENG_HOME' to your local Lia Engine home path.")
+        base_path = os.path.join(engine_home,'repositorys')
+        if not os.path.exists(base_path):
+            raise ValueError("Repository path does not exist.")
+
+        # file path
+        input_file = os.path.join(base_path,str(prj_id),'sqlsrc.dat')
+        if not os.path.exists(input_file):
+            raise ValueError("sqlsrc.dat file does not exist.")
+        output_file = os.path.join(base_path,str(prj_id),'sqlsrc.json')
+        if not os.path.exists(output_file):
+            raise ValueError("sqlsrc.json file does not exist.")
+
+        # pipeline setting
+        sqlsrc_file_pipeline_config = {
+            "source": {
+                "type": "sqlsrc_to_json_converter",
+                "config": {
+                    "input_path" : input_file,
+                    "output_path": output_file
+                }
+            },
+            "sink": {
+                "type": "console",
+            }
+        }
+
+        # run pipeline
+        pipeline = Pipeline.create(sqlsrc_file_pipeline_config)
+        pipeline.run()
+        pipeline.raise_from_status()
+    except Exception as e:
+        raise
+
+if __name__ == "__main__":
+    # example: prj_id = '21'
+    prj_id = 21
+    try:
+        convert_sqlsrc(prj_id=prj_id)
+    except Exception as e:
+        print(e)
