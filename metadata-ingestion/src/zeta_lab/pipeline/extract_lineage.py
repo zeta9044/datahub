6185d5099123344fe393218e85113fa1833742c8
kangyou.choi <kangyou.choi@datastreams.co.kr>
2024-10-25 13:29:11 +0900

    Remove old lineage.db file in extract_lineage function.
    Cleaned up the repository by ensuring that any existing `lineage.db` file is removed before creating a new one. This helps prevent potential conflicts arising from old lineage data. Also, minor formatting adjustments were made for improved code readability.

--- metadata-ingestion/src/zeta_lab/pipeline/extract_lineage.py
+++ metadata-ingestion/src/zeta_lab/pipeline/extract_lineage.py
@@ -3,7 +3,8 @@ import os
 from zeta_lab.utilities.tool import get_server_pid
 from datahub.ingestion.run.pipeline import Pipeline
 
-def extract_lineage(gms_server_url,prj_id):
+
+def extract_lineage(gms_server_url, prj_id):
     """
     :param gms_server_url: The URL of the GMS server to be checked for availability.
     :param prj_id: The project ID whose repository and sqlsrc.json file are required for lineage extraction.
@@ -21,20 +22,23 @@ def extract_lineage(gms_server_url,prj_id):
         engine_home = os.getenv("LIAENG_HOME")
         if not engine_home:
             raise ValueError("Please define environment variable 'LIAENG_HOME' to your local Lia Engine home path.")
-        repo_path = os.path.join(engine_home,'repositorys')
+        repo_path = os.path.join(engine_home, 'repositorys')
         if not os.path.exists(repo_path):
             raise ValueError("Repository path does not exist.")
-        prj_repo_path = os.path.join(repo_path,str(prj_id))
+        prj_repo_path = os.path.join(repo_path, str(prj_id))
         if not os.path.exists(prj_repo_path):
             raise ValueError(f"Project {prj_id} repository path does not exist.")
 
         # sqlsrc.json path
-        sqlsrc_json_path = os.path.join(prj_repo_path,'sqlsrc.json')
+        sqlsrc_json_path = os.path.join(prj_repo_path, 'sqlsrc.json')
         if not os.path.exists(sqlsrc_json_path):
             raise ValueError("sqlsrc.json file does not exist.")
 
         # lineage.db path on prj_repo_path
-        lineage_path = os.path.join(prj_repo_path,'lineage.db')
+        lineage_path = os.path.join(prj_repo_path, 'lineage.db')
+        if os.path.exists(lineage_path):
+            os.remove(lineage_path)
+            print(f"previous {lineage_path} has been removed.")
 
         duckdb_sink_config = {
             "type": "datahub-lite",
@@ -72,11 +76,12 @@ def extract_lineage(gms_server_url,prj_id):
     except Exception as e:
         raise
 
+
 if __name__ == "__main__":
     # example
     gms_server_url = "http://localhost:8000"
     prj_id = 21
     try:
-        extract_lineage(gms_server_url=gms_server_url,prj_id=prj_id)
+        extract_lineage(gms_server_url=gms_server_url, prj_id=prj_id)
     except Exception as e:
         print(e)
