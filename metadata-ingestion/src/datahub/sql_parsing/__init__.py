3341f115f008c99bb5ca2a9d539790218646dbc5
kangyou.choi <kangyou.choi@datastreams.co.kr>
2024-12-05 12:44:12 +0900

    Patch sqlglot's lineage function for custom behavior
    Introduce a monkey patch to sqlglot's lineage function, enabling customized lineage tracing through patched_lineage. This modification includes handling expressions and source expansion, ensuring compatibility with existing SQL parsing functions while adding flexibility for specific use cases.

---
+++ metadata-ingestion/src/datahub/sql_parsing/__init__.py
@@ -0,0 +1,4 @@
+# src/__init__.py
+from datahub.sql_parsing.patch_sqlglot import apply_patches
+
+apply_patches()
---
+++ metadata-ingestion/src/datahub/sql_parsing/patch_sqlglot.py
@@ -0,0 +1,55 @@
+import typing as t
+
+import sqlglot.lineage
+from sqlglot import Schema, exp, maybe_parse
+from sqlglot.errors import SqlglotError
+from sqlglot.lineage import Node, to_node
+from sqlglot.optimizer import Scope, build_scope, qualify
+
+if t.TYPE_CHECKING:
+    from sqlglot.dialects.dialect import DialectType
+# 기존 함수를 백업
+original_lineage = sqlglot.lineage.lineage
+
+def patched_lineage(
+        column: str | exp.Column,
+        sql: str | exp.Expression,
+        schema: t.Optional[t.Dict | Schema] = None,
+        sources: t.Optional[t.Mapping[str, str | exp.Query]] = None,
+        dialect: DialectType = None,
+        scope: t.Optional[Scope] = None,
+        trim_selects: bool = True,
+        **kwargs,
+) -> Node:
+    expression = maybe_parse(sql, dialect=dialect)
+    # column = normalize_identifiers.normalize_identifiers(column, dialect=dialect).name
+    assert isinstance(column, str)
+
+    if sources:
+        expression = exp.expand(
+            expression,
+            {k: t.cast(exp.Query, maybe_parse(v, dialect=dialect)) for k, v in sources.items()},
+            dialect=dialect,
+        )
+
+    if not scope:
+        expression = qualify.qualify(
+            expression,
+            dialect=dialect,
+            schema=schema,
+            **{"validate_qualify_columns": False, "identify": False, **kwargs},
+        )
+
+        scope = build_scope(expression)
+
+    if not scope:
+        raise SqlglotError("Cannot build lineage, sql must be SELECT")
+
+    if not any(select.alias_or_name == column for select in scope.expression.selects):
+        raise SqlglotError(f"Cannot find column '{column}' in query.")
+
+    return to_node(column, scope, dialect, trim_selects=trim_selects)
+
+# Monkey patch 적용
+def apply_patches():
+    sqlglot.lineage.lineage = patched_lineage
