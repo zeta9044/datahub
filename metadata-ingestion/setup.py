0b62cc6b0ed02bbbec555103c2dd112235e0a40b
kangyou.choi <kangyou.choi@datastreams.co.kr>
2025-02-06 13:45:49 +0900

    Downgrade sqlglot and add patchy dependency
    The sqlglot library has been downgraded to version 25.32.1 due to compatibility issues. Additionally, the patchy library (version 2.8.0) has been added as a new dependency to support future functionality.

--- metadata-ingestion/setup.py
+++ metadata-ingestion/setup.py
@@ -100,7 +100,8 @@ usage_common = {
 sqlglot_lib = {
     # Using an Acryl fork of sqlglot.
     # https://github.com/tobymao/sqlglot/compare/main...hsheth2:sqlglot:main?expand=1
-    "sqlglot[rs]==25.33.0",
+    "sqlglot[rs]==25.32.1",
+    "patchy==2.8.0",
 }
 
 classification_lib = {
